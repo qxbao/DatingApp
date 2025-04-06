5e487a79beb9297c4e6126c3fe7b982e9b0b60ff
Bao <dquanbao@gmail.com>
2025-04-06 23:04:11 +0700

    fix liker counting mechanic

--- src/main/java/fit/se2/datingapp/repository/UserSwipeRepository.java
+++ src/main/java/fit/se2/datingapp/repository/UserSwipeRepository.java
@@ -15,9 +15,11 @@ public interface UserSwipeRepository extends JpaRepository<UserSwipe, Long> {
     Optional<UserSwipe> findLikeSwipe(User liker, User liked);
     @Query("SELECT us.liked.id FROM UserSwipe us WHERE us.liker.id = :userId")
     List<Long> findSwipedUserIdsByUserId(Long userId);
-    @Query("SELECT us FROM UserSwipe us WHERE us.liked = :liked AND us.isLike = true AND NOT EXISTS" +
-            "(SELECT um FROM UserMatch um WHERE (um.user1 = us.liker AND um.user2 = :liked)" +
-            "OR (um.user1 = :liked AND um.user2 = us.liker))")
+    @Query("SELECT us FROM UserSwipe us WHERE us.liked = :liked AND us.isLike = true " +
+            "AND us.liker.role != 'BANNED' " +
+            "AND NOT EXISTS (SELECT um FROM UserMatch um WHERE " +
+            "(um.user1 = us.liker AND um.user2 = :liked) OR " +
+            "(um.user1 = :liked AND um.user2 = us.liker))")
     List<UserSwipe> findUserLikesByLiked(User liked);
 
     Optional<Object> findByLikerAndLiked(User a, User b);
